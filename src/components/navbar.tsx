76084f749dc18b313d17402293579d172a808d57
RyanGabr <ryanalexandregabriel@gmail.com>
2025-07-17 18:14:59 -0300

    Add repository link to 'GitHub' button in navigation bar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,10 +4,14 @@ export function Navbar() {
   return (
     <div className="w-full fixed top-0 flex items-center justify-between px-5 py-[14px]">
       <div>
-        <span className="font-semibold text-lg cursor-default">Focus Timer</span>
+        <span className="font-semibold text-lg cursor-default">
+          Focus Timer
+        </span>
       </div>
       <div>
-        <Button>GitHub</Button>
+        <a href="https://github.com/RyanGabr/focus-timer" target="_blank">
+          <Button>GitHub</Button>
+        </a>
       </div>
     </div>
   );
