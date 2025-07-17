59fbd7b1172c5a593ecb3eb1d25e30db6e560a68
RyanGabr <ryanalexandregabriel@gmail.com>
2025-07-17 19:40:04 -0300

    Add current time to page title and set favicon

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,24 @@
+import { useEffect } from "react";
 import { Navbar } from "./components/navbar";
+import { useTimer } from "./context/timer-context";
 import { Controls } from "./features/controls/controls";
 import { Presets } from "./features/presets/presets";
 import { Timer } from "./features/timer/timer";
 
 export function App() {
+  const { secondsAmount } = useTimer();
+
+  const minutes = Math.floor(secondsAmount / 60);
+  const seconds = secondsAmount % 60;
+
+  useEffect(() => {
+    document.title =
+      minutes.toString().padStart(2, "0") +
+      ":" +
+      seconds.toString().padStart(2, "0") +
+      " - Focus Timer";
+  }, [secondsAmount]);
+
   return (
     <div className="bg-zinc-950">
       <Navbar />
