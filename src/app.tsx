f2a521b56e2bf2036865a4fc672a9a5310770da4
RyanGabr <ryanalexandregabriel@gmail.com>
2025-07-17 14:21:57 -0300

    features: Timer

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,13 @@
 import { Navbar } from "./components/navbar";
+import { Timer } from "./features/timer/timer";
 
 export function App() {
   return (
     <div className="bg-zinc-950">
       <Navbar />
+      <main className="flex flex-col items-center justify-center gap-10 h-screen">
+        <Timer />
+      </main>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { App } from './app'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./app";
+import { TimerProvider } from "./context/timer-context";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <TimerProvider>
+      <App />
+    </TimerProvider>
+  </StrictMode>
+);
